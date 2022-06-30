463d7f7b9d135348e036092a58d0efbc6e87bd83
Tan Yong <asleepychild@gmail.com>
2022-06-30 14:55:27 +0800

    fix a bug

--- src/App.vue
+++ src/App.vue
@@ -27,7 +27,7 @@ export default {
 </script>
 
 <style>
-html, body {
+html, body, #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
